bba60938abc722e814d2194251998d26c16688c8
fezproof <26830309+fezproof@users.noreply.github.com>
2024-02-04 09:58:57 +0800

    Delete some folders that should be git ignored

--- supabase/node_modules/@supabase/gotrue-js/dist/module/index.js
+++
@@ -1,7 +0,0 @@
-import GoTrueAdminApi from './GoTrueAdminApi';
-import GoTrueClient from './GoTrueClient';
-export { GoTrueAdminApi, GoTrueClient };
-export * from './lib/types';
-export * from './lib/errors';
-export { navigatorLock, NavigatorLockAcquireTimeoutError, internals as lockInternals, } from './lib/locks';
-//# sourceMappingURL=index.js.map
