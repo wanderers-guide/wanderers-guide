76090e1f85a720f92cbf56f277fb8afb392367eb
Aaron Cassar <aaron@sellscale.com>
2024-03-21 19:33:04 -0700

    Added update user

---
+++ supabase/functions/update-user/index.ts
@@ -0,0 +1,39 @@
+// @ts-ignore
+import { serve } from 'std/server';
+import { connect, getPublicUser, updateData } from '../_shared/helpers.ts';
+import type { PublicUser } from '../_shared/content';
+
+serve(async (req: Request) => {
+  return await connect(req, async (client, body) => {
+    let { display_name, summary, image_url, background_image_url, site_theme } = body as PublicUser;
+
+    const user = await getPublicUser(client);
+
+    if (!user) {
+      return {
+        status: 'error',
+        message: 'User not found',
+      };
+    }
+
+    const status = await updateData(client, 'public_user', user.id, {
+      display_name,
+      summary,
+      image_url,
+      background_image_url,
+      site_theme,
+    });
+
+    if (status === 'SUCCESS') {
+      return {
+        status: 'success',
+        data: true,
+      };
+    } else {
+      return {
+        status: 'error',
+        message: status,
+      };
+    }
+  });
+});
