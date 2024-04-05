d78789d5a68af14ad9168941bcf4d74a781d80f9
Vytor Calixto <vytor.bezerra@azos.com.br>
2024-04-05 10:12:01 -0300

    chore: data dump

--- data/dump.sh
+++ data/dump.sh
@@ -5,3 +5,6 @@ export PGCLUSTER=15/main
 
 # Dump the schema only
 pg_dump --schema-only --table="public.*" "$1" > schema.sql
+
+# Dump the data
+pg_dump --data-only --table="public.*" --exclude-table="public.public_user" "$1" > data.sql
