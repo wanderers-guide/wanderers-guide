03f11856c3edbe186b92c3fa229c8efe8ef7b637
Vytor Calixto <vytor.bezerra@azos.com.br>
2024-04-09 17:23:13 -0300

    fix: drop public schema before creating local database

--- data/create-db.sh
+++ data/create-db.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo Dropping schema
+psql -d "$1" -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public; GRANT ALL ON SCHEMA public TO postgres; GRANT ALL ON SCHEMA public TO public;"
+
 echo Recreating schema
 psql  -d "$1" -a -f schema.sql
 
