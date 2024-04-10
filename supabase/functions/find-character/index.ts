0401f5123ab96e322b78a84f4646b9a3894bad28
Vytor Calixto <vytor.bezerra@azos.com.br>
2024-04-10 10:49:42 -0300

    chore: allow to find characters by campaign id

--- supabase/functions/find-character/index.ts
+++ supabase/functions/find-character/index.ts
@@ -1,26 +1,27 @@
 // @ts-ignore
 import { serve } from 'std/server';
-import { connect, fetchData } from '../_shared/helpers.ts';
 import type { Character } from '../_shared/content';
+import { connect, fetchData } from '../_shared/helpers.ts';
+
+interface FindCharacterBody {
+  id?: number | number[];
+  user_id?: string;
+  campaign_id?: number;
+}
 
 serve(async (req: Request) => {
   return await connect(req, async (client, body) => {
-    let { id, user_id } = body as {
-      id?: number | number[];
-      user_id?: string;
-    };
+    let { id, user_id, campaign_id } = body as FindCharacterBody;
 
     const results = await fetchData<Character>(client, 'character', [
       { column: 'id', value: id },
       { column: 'user_id', value: user_id },
+      { column: 'campaign_id', value: campaign_id },
     ]);
 
-    const data =
-      id === undefined || Array.isArray(id)
-        ? results.sort((a, b) => a.id - b.id)
-        : results.length > 0
-        ? results[0]
-        : null;
+    const data = results.length == 1
+      ? results[0]
+      : results.sort((a, b) => a.id - b.id);
     return {
       status: 'success',
       data,
