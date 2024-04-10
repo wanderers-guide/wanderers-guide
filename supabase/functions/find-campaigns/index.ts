299547af9377f345895225846bd026f865128e19
Vytor Calixto <vytor.bezerra@azos.com.br>
2024-04-10 10:32:47 -0300

    feat: add function to get campaigns

---
+++ supabase/functions/find-campaigns/index.ts
@@ -0,0 +1,31 @@
+// @ts-ignore
+import { serve } from 'std/server';
+import type { Campaign } from '../_shared/content';
+import { connect, fetchData } from '../_shared/helpers.ts';
+
+interface FindCampaignsBody {
+  id?: number | number[];
+  user_id?: string;
+  join_key?: string;
+}
+
+serve(async (req: Request) => {
+  return await connect(req, async (client, body) => {
+    let { id, user_id, join_key } = body as FindCampaignsBody;
+
+    const results = await fetchData<Campaign>(client, 'campaign', [
+      { column: 'id', value: id },
+      { column: 'user_id', value: user_id },
+      { column: 'join_key', value: join_key }
+    ]);
+
+    const data = results.length == 1
+      ? results[0]
+      : results.sort((a, b) => a.id - b.id);
+
+    return {
+      status: 'success',
+      data,
+    };
+  });
+});
