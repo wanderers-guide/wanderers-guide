4063ee9a361587517c43eafa92e6bf15282f333c
Aaron Cassar <aaron@sellscale.com>
2024-04-16 01:44:30 -0700

    Added forced movement

--- frontend/src/process/conditions/condition-handler.ts
+++ frontend/src/process/conditions/condition-handler.ts
@@ -376,6 +376,16 @@ const CONDITIONS: Condition[] = [
     for_creature: false,
     for_object: false,
   },
+  {
+    name: 'Forced Movement',
+    description: `When an effect forces you to move, or if you start falling, the distance you move is defined by the effect that moved you, not by your Speed. Forced movement doesn't trigger reactions that are triggered by movement. In the rare cases where it's unclear whether your movement is voluntary or forced, the GM makes the determination.
+    If forced movement would move you into a space you can't occupy—because objects are in the way or because you lack the movement type needed to reach it, for example—you stop moving in the last space you can occupy.
+    Usually the creature or effect forcing the movement chooses the path the victim takes. If you're pushed or pulled, you can usually be moved through hazardous terrain, pushed off a ledge, or the like. Abilities that reposition you in some other way can't put you in such dangerous places unless they specify otherwise. In all cases, the GM makes the final call if there's doubt on where forced movement can move a creature.
+    Some abilities allow a creature to move while carrying another along with it. This is forced movement for the carried creature. Unless noted otherwise, they both move on the same path while this happens—the carrying creature can't drag its victim through dangers while avoiding them itself, for example.`,
+    for_character: false,
+    for_creature: false,
+    for_object: false,
+  },
 ];
 
 export function getConditionByName(name: string, addedSource?: string): Condition | undefined {
