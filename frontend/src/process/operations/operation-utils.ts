711ce4bba0e224c7754e3ffb925d79cd8d65a58a
a-hodges <16946799+a-hodges@users.noreply.github.com>
2024-08-20 17:09:48 -0700

    Add display for feat selection operations

--- frontend/src/process/operations/operation-utils.ts
+++ frontend/src/process/operations/operation-utils.ts
@@ -250,7 +250,10 @@ export const hasOperationSelection = (result: OperationResult) => {
   return false;
 };
 
-export function getSelectedOption(entity: LivingEntity | null, op: Operation): OperationSelectOptionCustom | null {
+export function getSelectedCustomOption(
+  entity: LivingEntity | null,
+  op: Operation
+): OperationSelectOptionCustom | null {
   if (!entity) return null;
   if (op.type === 'select' && op.data.modeType === 'PREDEFINED' && op.data.optionType === 'CUSTOM') {
     // Custom select option
@@ -263,6 +266,35 @@ export function getSelectedOption(entity: LivingEntity | null, op: Operation): O
     ? op.data.optionsPredefined?.find((option) => option.id === entity!.operation_data!.selections![selectionKey])
     : null;
   return (selectedOption as OperationSelectOptionCustom) ?? null;
+}
+
+export async function getSelectedOption(
+  entity: LivingEntity | null,
+  op: OperationSelect
+): Promise<OperationSelectOptionCustom | ObjectWithUUID | null> {
+  if (!entity) return null;
+  if (op.type === 'select' && op.data.modeType === 'PREDEFINED' && op.data.optionType === 'CUSTOM') {
+    // Custom select option
+    const selectionKey = Object.keys(entity?.operation_data?.selections ?? {}).find((key) => key.endsWith(op.id));
+    const selectedOption = selectionKey
+      ? op.data.optionsPredefined?.find((option) => option.id === entity!.operation_data!.selections![selectionKey])
+      : null;
+    return (selectedOption as OperationSelectOptionCustom) ?? null;
+  } else {
+    const selectionKey = Object.keys(entity?.operation_data?.selections ?? {}).find((key) => key.endsWith(op.id));
+    if (!selectionKey) {
+      return null;
+    }
+    const options = await determineFilteredSelectionList(
+      op.data.optionType,
+      op.id,
+      (op.data.optionsFilters ?? []) as OperationSelectFilters
+    );
+    const selectedOption = options.find(
+      (option) => option._select_uuid === entity!.operation_data!.selections![selectionKey]
+    );
+    return (selectedOption as ObjectWithUUID) ?? null;
+  }
 }
 
 export function convertKeyToBasePrefix(key: string, id?: number): string {
