5b0f9832f879ca9296b3a55e8fd9d18f62e0452a
Quzzar <aaronncassar@gmail.com>
2024-09-21 23:48:17 -0400

    Added delays to next page clicks

--- frontend/cypress/e2e/login/login.cy.ts
+++ frontend/cypress/e2e/login/login.cy.ts
@@ -1,24 +1,27 @@
 describe('Login', () => {
-  beforeEach( () => {
+  beforeEach(() => {
     cy.visit('/');
   });
 
   it('should login', () => {
     cy.get('button').contains('Sign in').click();
+    cy.wait(500);
     cy.location('pathname').should('eq', '/login');
 
     cy.get('input[name="email"]').type(Cypress.env('TEST_EMAIL'));
     cy.get('input[name="password"]').type(Cypress.env('TEST_PASSWORD'));
     cy.get('button[type="submit"]').contains('Sign in').click();
-    cy.location('pathname', {timeout: 1000}).should('eq', '/characters');
+    cy.wait(500);
+    cy.location('pathname', { timeout: 1000 }).should('eq', '/characters');
   });
 
   it('should show error message for invalid credentials', () => {
     cy.get('button').contains('Sign in').click();
+    cy.wait(500);
     cy.location('pathname').should('eq', '/login');
 
-    cy.get('input[name="email"]').type("foo@bar.com");
-    cy.get('input[name="password"]').type("wrongpassword");
+    cy.get('input[name="email"]').type('foo@bar.com');
+    cy.get('input[name="password"]').type('wrongpassword');
     cy.get('button[type="submit"]').contains('Sign in').click();
     cy.contains('Invalid login credentials').should('exist');
   });
