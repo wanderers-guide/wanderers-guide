85f2270856d58cf2cd4fd24f0cd13e8447a6e35a
Quzzar <aaronncassar@gmail.com>
2025-01-23 22:43:42 -0500

    Improved conditional displaying

--- frontend/src/process/variables/variable-display.tsx
+++ frontend/src/process/variables/variable-display.tsx
@@ -56,7 +56,7 @@ export function displayFinalProfValue(
   return (
     <span style={{ position: 'relative' }}>
       {parts.hasConditionals ? (
-        <HoverCard shadow='md' openDelay={1000} width={230} position='bottom' withArrow>
+        <HoverCard shadow='md' openDelay={500} width={230} position='bottom' withArrow>
           <HoverCard.Target>
             <span>
               {<>{value}</>}
@@ -75,22 +75,22 @@ export function displayFinalProfValue(
             </span>
           </HoverCard.Target>
           <HoverCard.Dropdown py={5} px={10}>
-            <Text c='gray.0' size='xs'>
-              You have some conditionals! These will only apply situationally:
-              <Divider pb={5} />
-              <List size='xs'>
-                {parts.breakdown.conditionals.map((item, i) => (
-                  <List.Item key={i}>
-                    {item.text}
-                    <br />
-                    <Text c='dimmed' span>
-                      {'['}from {item.source}
-                      {']'}
-                    </Text>
-                  </List.Item>
-                ))}
-              </List>
-            </Text>
+            <Box mr={10} my={5}>
+              <Text c='gray.0' size='xs'>
+                <List size='xs'>
+                  {parts.breakdown.conditionals.map((item, i) => (
+                    <List.Item key={i}>
+                      {item.text}
+                      <br />
+                      <Text c='dimmed' span>
+                        {'['}from {item.source}
+                        {']'}
+                      </Text>
+                    </List.Item>
+                  ))}
+                </List>
+              </Text>
+            </Box>
           </HoverCard.Dropdown>
         </HoverCard>
       ) : (
@@ -101,24 +101,55 @@ export function displayFinalProfValue(
 }
 
 export function displayFinalVariableValue(id: StoreID, variableName: string) {
+  const variable = getVariable<VariableProf>(id, variableName);
+  if (!variable) return null;
+
   const finalData = getFinalVariableValue(id, variableName);
   const breakdown = getVariableBreakdown(id, variableName);
 
   return (
     <span style={{ position: 'relative' }}>
-      {<>{finalData.total}</>}
       {breakdown.conditionals.length > 0 ? (
-        <Text
-          c='guide.5'
-          style={{
-            position: 'absolute',
-            top: -6,
-            right: -7,
-          }}
-        >
-          *
-        </Text>
-      ) : null}
+        <HoverCard shadow='md' openDelay={500} width={230} position='bottom' withArrow>
+          <HoverCard.Target>
+            <span>
+              {<>{finalData.total}</>}
+              {breakdown.conditionals.length > 0 ? (
+                <Text
+                  c='guide.5'
+                  style={{
+                    position: 'absolute',
+                    top: -6,
+                    right: -7,
+                  }}
+                >
+                  *
+                </Text>
+              ) : null}
+            </span>
+          </HoverCard.Target>
+          <HoverCard.Dropdown py={5} px={10}>
+            <Box mr={10} my={5}>
+              <Text c='gray.0' size='xs'>
+                <List size='xs'>
+                  {breakdown.conditionals.map((item, i) => (
+                    <List.Item key={i}>
+                      {item.text}
+                      <br />
+                      <Text c='dimmed' span>
+                        {'['}from {item.source}
+                        {']'}
+                      </Text>
+                    </List.Item>
+                  ))}
+                </List>
+              </Text>
+            </Box>
+          </HoverCard.Dropdown>
+        </HoverCard>
+      ) : (
+        <span>{finalData.total}</span>
+      )}
     </span>
   );
 }
@@ -326,19 +357,47 @@ export function displayFinalAcValue(id: StoreID, item?: Item) {
 
   return (
     <span style={{ position: 'relative' }}>
-      {<>{value}</>}
       {parts.hasConditionals ? (
-        <Text
-          c='guide.5'
-          style={{
-            position: 'absolute',
-            top: -6,
-            right: -7,
-          }}
-        >
-          *
-        </Text>
-      ) : null}
+        <HoverCard shadow='md' openDelay={500} width={230} position='bottom' withArrow>
+          <HoverCard.Target>
+            <span>
+              {<>{value}</>}
+              {parts.hasConditionals ? (
+                <Text
+                  c='guide.5'
+                  style={{
+                    position: 'absolute',
+                    top: -6,
+                    right: -7,
+                  }}
+                >
+                  *
+                </Text>
+              ) : null}
+            </span>
+          </HoverCard.Target>
+          <HoverCard.Dropdown py={5} px={10}>
+            <Box mr={10} my={5}>
+              <Text c='gray.0' size='xs'>
+                <List size='xs'>
+                  {parts.breakdown.conditionals.map((item, i) => (
+                    <List.Item key={i}>
+                      {item.text}
+                      <br />
+                      <Text c='dimmed' span>
+                        {'['}from {item.source}
+                        {']'}
+                      </Text>
+                    </List.Item>
+                  ))}
+                </List>
+              </Text>
+            </Box>
+          </HoverCard.Dropdown>
+        </HoverCard>
+      ) : (
+        <span>{value}</span>
+      )}
     </span>
   );
 }
@@ -391,19 +450,47 @@ export function displayFinalSpeedValue(id: StoreID, variableName: string, entity
 
   return (
     <span style={{ position: 'relative' }}>
-      {<>{finalData.total}</>}
       {breakdown.conditionals.length > 0 ? (
-        <Text
-          c='guide.5'
-          style={{
-            position: 'absolute',
-            top: -6,
-            right: -7,
-          }}
-        >
-          *
-        </Text>
-      ) : null}
+        <HoverCard shadow='md' openDelay={500} width={230} position='bottom' withArrow>
+          <HoverCard.Target>
+            <span>
+              {<>{finalData.total}</>}
+              {breakdown.conditionals.length > 0 ? (
+                <Text
+                  c='guide.5'
+                  style={{
+                    position: 'absolute',
+                    top: -6,
+                    right: -7,
+                  }}
+                >
+                  *
+                </Text>
+              ) : null}
+            </span>
+          </HoverCard.Target>
+          <HoverCard.Dropdown py={5} px={10}>
+            <Box mr={10} my={5}>
+              <Text c='gray.0' size='xs'>
+                <List size='xs'>
+                  {breakdown.conditionals.map((item, i) => (
+                    <List.Item key={i}>
+                      {item.text}
+                      <br />
+                      <Text c='dimmed' span>
+                        {'['}from {item.source}
+                        {']'}
+                      </Text>
+                    </List.Item>
+                  ))}
+                </List>
+              </Text>
+            </Box>
+          </HoverCard.Dropdown>
+        </HoverCard>
+      ) : (
+        <span>{finalData.total}</span>
+      )}
     </span>
   );
 }
