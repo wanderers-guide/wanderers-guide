01fb0ad935fa5667e4d16fe948916ea62386c74a
Quzzar <aaronncassar@gmail.com>
2025-06-22 19:53:11 -0400

    Fixed crpyress tests

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   env: {
-    "functions_url": "http://127.0.0.1:54321/functions/v1"
+    functions_url: 'http://127.0.0.1:54321/functions/v1',
   },
   e2e: {
+    baseUrl: 'http://localhost:5173',
+    pageLoadTimeout: 120000,
     // defaultCommandTimeout: 25000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
