3a83fa4599615981b9f3d06709116bed23c0f4d7
Quzzar <aaronncassar@gmail.com>
2026-01-07 13:50:15 -0500

    Updated other traits

--- frontend/src/utils/character.ts
+++ frontend/src/utils/character.ts
@@ -1,8 +1,6 @@
 import { Character } from '@typing/content';
 
 export function isPlayable(character?: Character | null) {
-  console.log('isPlayable check:', character);
-
   if (!character) return false;
   return !!(
     character.level > 0 &&
