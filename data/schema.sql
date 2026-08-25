d3ecb55b69c0738ae54bd74f5e79c4d5ee9ab49d
Quzzar <31683740+Quzzar@users.noreply.github.com>
2026-08-25 09:43:35 +0000

    [no ci] Update Schema

--- data/schema.sql
+++ data/schema.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict WQ47gaf0CKGSwgKmaNc2ZpXWYKrorZYQD5inEH3ylb40b9LthPWXzgKMoVpPguG
+\restrict GHK0NkEkJmxqgozJkQseCdazXmexuG3z6npJ1uP8hCEDgXgqm5cTuN8JJX11sae
 
 -- Dumped from database version 15.1 (Ubuntu 15.1-1.pgdg20.04+1)
 -- Dumped by pg_dump version 16.15 (Ubuntu 16.15-1.pgdg24.04+2)
@@ -2966,5 +2966,5 @@ GRANT ALL ON SEQUENCE public.versatile_heritage_id_seq TO service_role;
 -- PostgreSQL database dump complete
 --
 
-\unrestrict WQ47gaf0CKGSwgKmaNc2ZpXWYKrorZYQD5inEH3ylb40b9LthPWXzgKMoVpPguG
+\unrestrict GHK0NkEkJmxqgozJkQseCdazXmexuG3z6npJ1uP8hCEDgXgqm5cTuN8JJX11sae
 
