3a78c9e009d0b4156ed3e8afaf2c4075af78d098
Aaron Cassar <aaronncassar@gmail.com>
2026-09-06 00:42:19 -0400

    Isolate service-worker caching from functional Cypress tests

--- frontend/cypress/support/e2e.ts
+++ frontend/cypress/support/e2e.ts
@@ -16,5 +16,16 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+// Cypress 13 cannot instrument HTML returned from a service-worker cache, causing
+// cy.visit() to hang even though the page rendered. Keep functional request/recovery
+// tests on the network; browser-recovery.test.mjs exercises the actual update lifecycle.
+// https://github.com/cypress-io/cypress/issues/30405
+const isolateServiceWorker = () => cy.intercept('GET', '**/sw.js', {
+  body: '/* Functional test worker: no cache or fetch interception. */',
+  headers: { 'content-type': 'application/javascript', 'cache-control': 'no-store' },
+});
+before(isolateServiceWorker);
+beforeEach(isolateServiceWorker);
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
